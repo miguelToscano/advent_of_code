9e884512ec8bc2be644dfd2dc8fd4f3ad83b74d1
miguelToscano <miguetoscano288@gmail.com>
2022-12-03 11:43:11 -0300

    Completed part 1 for day 3

---
+++ day_3/target/debug/deps/day_3-029e5c3d83494bf6.d
@@ -0,0 +1,6 @@
+/Users/migue/Documents/advent_of_code/day_3/target/debug/deps/day_3-029e5c3d83494bf6.rmeta: src/main.rs src/part_1.rs
+
+/Users/migue/Documents/advent_of_code/day_3/target/debug/deps/day_3-029e5c3d83494bf6.d: src/main.rs src/part_1.rs
+
+src/main.rs:
+src/part_1.rs:
---
+++ day_3/target/debug/deps/day_3-30bcbf620fd78be2.d
@@ -0,0 +1,6 @@
+/Users/migue/Documents/advent_of_code/day_3/target/debug/deps/day_3-30bcbf620fd78be2: src/main.rs src/part_1.rs
+
+/Users/migue/Documents/advent_of_code/day_3/target/debug/deps/day_3-30bcbf620fd78be2.d: src/main.rs src/part_1.rs
+
+src/main.rs:
+src/part_1.rs:
